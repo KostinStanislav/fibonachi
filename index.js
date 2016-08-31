d539c826b4490486f0d421a66898ff44c50b7d65
KostinStanislav <tigrarion@mail.ru>
2016-08-31 16:20:29 +0000

    new

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var fib=[0,1];
 var n=1;
-while (n<15){
+var a=Number(prompt('Enter The Fibonacci position number'));
+while (n<a){
     fib.push(Number(fib[n]+fib[n-1]));
     Number(n++);
 }
 console.log(fib);
-document.write(fib[prompt('Enter The Fibonacci position number')]);
+document.write(fib[a]);
